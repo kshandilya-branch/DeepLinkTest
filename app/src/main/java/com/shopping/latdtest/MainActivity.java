a6ce18098260b2773ecaf88ee09966cb96c61779
Kartik Shandilya <kartik.shandilya@branch.io>
2021-05-10 16:45:33 +0530

    Initial commit

---
+++ app/src/main/java/com/shopping/latdtest/MainActivity.java
@@ -0,0 +1,44 @@
+package com.shopping.latdtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONObject;
+
+import io.branch.referral.Branch;
+import io.branch.referral.BranchError;
+import io.branch.referral.ServerRequestGetLATD;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Branch.sessionBuilder(this).withCallback(branchReferralInitListener).withData(getIntent() != null ? getIntent().getData() : null).init();
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Branch.sessionBuilder(this).withCallback(branchReferralInitListener).reInit();
+    }
+
+    private Branch.BranchReferralInitListener branchReferralInitListener = (linkProperties, error) -> {
+        // do stuff with deep link data (nav to page, display content, etc)
+        if(error!=null){
+            Log.d("Branch", ": "+error);
+        }
+        Log.d("SDK response : ", "onInitFinished: " + linkProperties.toString());
+    };
+
+}
